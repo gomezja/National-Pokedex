5266c20d8d77d99b2a16304903dc3437e75583b4
Jesus Gomez <gomezja@Jesuss-MacBook-Pro.local>
2017-07-13 14:22:31 -0700

    started ui for app

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,31 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Jesus Gomez on 7/12/17.
+//  Copyright © 2017 gomezja. All rights reserved.
+//
+
+import Foundation
+
+// this class will hold all of our pokemon
+class Pokemon {
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    // **** getters ****
+    var name: String {
+        return _name
+    }
+    
+    var pokedexID: Int {
+        return _pokedexID
+    }
+    // *****************
+    
+    init(name: String, pokedexID: Int) {
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+    
+}
